704a8f30fa102945bfc4919e6a0010c807d331e0
Mohammad Pezeshki <mohammadpz@gmail.com>
2015-03-13 15:42:28 -0400

    assert added

--- tests/monitoring/test_non_theano_monitoring.py
+++ tests/monitoring/test_non_theano_monitoring.py
@@ -6,30 +6,38 @@ from numpy.testing import assert_raises
 from blocks.graph import ComputationGraph
 from blocks.monitoring.evaluators import DatasetEvaluator
 from tests.monitoring.test_aggregation import TestBrick
+from blocks.monitoring.aggregation import MonitoredQuantity
 from blocks.bricks.cost import CategoricalCrossEntropy
-from blocks.extensions.monitoring import CrossEntropy
 
 floatX = theano.config.floatX
+
+
+class CrossEntropy(MonitoredQuantity):
+    def __init__(self, **kwargs):
+        super(CrossEntropy, self).__init__(**kwargs)
+        self.total_cross_entropy, self.examples_seen = 0.0, 0
+
+    def accumulate(self, target, predicted):
+        import numpy
+        self.total_cross_entropy += -(target * numpy.log(predicted)).sum()
+        self.examples_seen += 1
+
+    def readout(self):
+        return self.total_cross_entropy / self.examples_seen
 
 
 def test_dataset_evaluators():
     X = theano.tensor.vector('X')
     Y = theano.tensor.vector('Y')
 
-    # Three batches, each with 2 elements
-    data = [numpy.arange(1, 7, dtype=floatX).reshape(3, 2) / 7.0,
-            numpy.arange(11, 17, dtype=floatX).reshape(3, 2) / 17.0]
+    data = [numpy.arange(1, 7, dtype=floatX).reshape(3, 2),
+            numpy.arange(11, 17, dtype=floatX).reshape(3, 2)]
+    data_stream = IterableDataset(dict(X=data[0],
+                                       Y=data[1])).get_example_stream()
 
-    data_stream = IterableDataset(dict(X=data[0], Y=data[1])).get_example_stream()
-
-    print data
-
-    validator = DatasetEvaluator([CrossEntropy(requires=[X, Y], 
+    validator = DatasetEvaluator([CrossEntropy(requires=[X, Y],
         name="non_thenao_cross_entropy"), 
         CategoricalCrossEntropy().apply(X, Y), ])
     values = validator.evaluate(data_stream)
-    print values
-
-
-if __name__ == '__main__':
-    test_dataset_evaluators()
+    numpy.testing.assert_allclose(values['non_thenao_cross_entropy'],
+        values['categoricalcrossentropy_apply_cost'])
